4c5c1e26fd66c2b1f3952027418395ccbc5c83d3
lvh0301 <levanhieu0301@gmail.com>
2026-06-24 09:43:31 +0700

    Khởi tạo dự án

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Ok122")
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 4000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Website đang chạy trên cổng ${port}`);
+});
